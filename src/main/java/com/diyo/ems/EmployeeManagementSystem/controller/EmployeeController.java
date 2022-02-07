4417d2bb375636bf1c1bf56d627cd5d053e3668e
pjabhan <pbhandari1019@gmail.com>
2022-02-07 17:01:27 -0600

    Added a post mapping for getting id

--- src/main/java/com/diyo/ems/EmployeeManagementSystem/controller/EmployeeController.java
+++ src/main/java/com/diyo/ems/EmployeeManagementSystem/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.diyo.ems.EmployeeManagementSystem.entity.Employee;
 import com.diyo.ems.EmployeeManagementSystem.repository.EmployeeRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,10 @@ public class EmployeeController {
 
     @GetMapping("/getById")
     public String getEmployeeById(){
+        return  "Hello world";
+    }
+    @PostMapping("/getId")
+    public String getEmployeeId(){
         return  "Hello world";
     }
 }
